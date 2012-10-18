3e2ef7bb5ec7aface0ebd856e3a9f71097760fe4
Daniel Bahr <dbahr92@gmail.com>
2012-10-18 12:26:44 -0700

    Added capability to parse JSON and talk with the web server.

--- Game_Engine/Assets/Scripts/Networking Module/NetworkInterface.cs
+++ Game_Engine/Assets/Scripts/Networking Module/NetworkInterface.cs
@@ -4,38 +4,127 @@ using System.Collections.Generic;
 using System.Net;
 using System;
 
-public class NetworkInterface
+public class NetworkInterface : MonoBehaviour
 {
-    private delegate string ErrorHandler();
-
-    private void SendGETRequest(string url)
+    void Start()
     {
-        using (WebClient wc = new WebClient())
+        JSONObject obj = new JSONObject("{\"turrets\":[{\"ID\":\"1\",\"position\":\"700,75,700\"},{\"ID\":\"2\",\"position\":\"200,75,200\"},{\"ID\":\"3\",\"position\":\"1400,75,1400\"}]}");
+        foreach (JSONObject turret in obj["turrets"].list)
         {
-            wc.DownloadStringAsync(new Uri(url));
+            GameObject newT = Instantiate(m_turretClone, ParseVector3(turret["position"].str), Quaternion.identity) as GameObject;
+            newT.name = ("turret" + turret["ID"].str);
+        }
+
+        //StartCoroutine("PollData");
+    }
+
+    private IEnumerator PollData()
+    {
+        float startTime;
+        while (true)
+        {
+            startTime = Time.realtimeSinceStartup;
+            WWW get = new WWW(m_serverURL);
+            yield return get;
+            Debug.Log(get.text);
+            HandleJSONTurret(get.text);
+            yield return new WaitForSeconds(Mathf.Clamp((1.0f / m_pollRate) - (Time.realtimeSinceStartup - startTime), 0, 1));
         }
     }
 
-    private void SendPOSTRequest(string url, string data)
+    private void HandleJSONTurret(string s)
     {
-        using (WebClient wc = new WebClient())
+        JSONObject obj = new JSONObject(s);
+        if (obj == null || obj.type == JSONObject.Type.NULL)
+            return;
+
+        JSONObject jsonTurret = obj["turret"];
+
+        if (jsonTurret == null || jsonTurret.type == JSONObject.Type.NULL)
+            return;
+
+        JSONObject jsonID = jsonTurret["ID"];
+        JSONObject.Type jsonIDType = jsonID.type;
+        if (jsonID == null || jsonIDType == JSONObject.Type.NULL)
+            return;
+
+        JSONObject jsonPosition = jsonTurret["position"];
+        if (jsonPosition == null || jsonPosition.type != JSONObject.Type.STRING)
+            return;
+
+        Vector3 position;
+        try
         {
-            wc.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
-            wc.UploadStringAsync(new Uri(url), "POST", data);
+            position = ParseVector3(jsonPosition.str);
+        }
+        catch (InvalidOperationException)
+        {
+            return;
+        }
+
+        string turretNameAndID;
+        if (jsonIDType == JSONObject.Type.NUMBER)
+            turretNameAndID = "turret" + jsonID.n.ToString();
+        else
+            turretNameAndID = "turret" + jsonID.str;
+
+        GameObject turret;
+        if (TryGetObject(turretNameAndID, out turret) == true)
+        {
+            turret.transform.position = position;
+        }
+        else
+        {
+            turret = Instantiate(m_turretClone, position, Quaternion.identity) as GameObject;
+            turret.name = turretNameAndID;
         }
     }
 
-    private string PositionToPOSTData(Vector3 vec)
+    private static Vector3 ParseVector3(string s)
     {
-        return "position=" + vec.x + "," + vec.y + "," + vec.z;
+        char[] comma = { ',' };
+        string[] nums = s.Split(comma, 3);
+
+        if (nums.Length != 3)
+            throw new InvalidOperationException();
+        else
+        {
+            Vector3 retVec = Vector3.zero;
+            float coord;
+
+            if (float.TryParse(nums[0], out coord) == false)
+                throw new InvalidOperationException();
+            retVec.x = coord;
+
+            if (float.TryParse(nums[1], out coord) == false)
+                throw new InvalidOperationException();
+            retVec.y = coord;
+
+            if (float.TryParse(nums[2], out coord) == false)
+                throw new InvalidOperationException();
+            retVec.z = coord;
+
+            return retVec;
+        }
     }
 
-    public void SendCharacterData(Vector3 position)
+    private static bool TryGetObject(string name, string id, out GameObject foundObject)
     {
-        SendPOSTRequest("url", position.ToString());
+        return TryGetObject(name + id, out foundObject);
     }
 
-    public void SendData()
+    private static bool TryGetObject(string nameAndID, out GameObject foundObject)
     {
+        foundObject = GameObject.Find(nameAndID);
+        return (foundObject != null);
     }
+
+    [SerializeField]
+    private string m_serverURL;
+
+    [SerializeField]
+    private int m_pollRate;
+
+    [SerializeField]
+    private GameObject m_turretClone;
 }
